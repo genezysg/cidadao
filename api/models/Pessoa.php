81c44327af6b39a45030e019080f986fe34a4739
Genezys <genezys@gmail.com>
2015-06-03 10:25:10 -0300

    Adiciona variaveis publicas no model

--- api/models/Pessoa.php
+++ api/models/Pessoa.php
@@ -1,9 +1,12 @@
 <?php
 
-namespace Entity;
-use Phalcon\Mvc\Models;
+//namespace Entity;
+use Phalcon\Mvc\Model;
 
 
 class Pessoa extends Model{
+public $id;
+public $nome;
+public $nacionalidade;
 
 }
