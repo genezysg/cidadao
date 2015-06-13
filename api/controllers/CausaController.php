d6e3ccd60220995c3d646c4342937060287d9c0c
Genezys <genezys@gmail.com>
2015-06-13 11:06:13 -0300

    Verifica portabilidade de funcionalidades para controller

--- api/controllers/CausaController.php
+++ api/controllers/CausaController.php
@@ -12,5 +12,14 @@ public function getAll(){
   echo json_encode("hue");
 }
 
+public function post(){
+  $value=$this->request->getJsonRawBody();
+  $value->hue="worked";
+  echo json_encode($value);
+}
+
+public function getOne($id){
+  echo json_encode("hue".$id);
+}
 
 }
--- api/routes/Routes.php
+++ api/routes/Routes.php
@@ -9,7 +9,16 @@ $causaCollection->setHandler(new CausaController());
 
 $causaCollection->setPrefix('/causa');
 
+//Agrupar por rota "/"
 $causaCollection->get('/', 'getAll');
+
+$causaCollection->post('/', 'post');
+
+$causaCollection->options('/', 'getAll');
+
+//Agrupar por rota "/{id}"
+$causaCollection->get('/{id}', 'getOne');
+
 
 $app->mount($causaCollection);
 //Finish the causa controller routers
