aec69a48ead8df1bfed25fd0eaf8c79e4d1c9aeb
mathgarcia <mathmgarcia@gmail.com>
2015-06-21 17:02:58 -0300

    Corrigindo API

--- api/controllers/CausaController.php
+++ api/controllers/CausaController.php
@@ -1,32 +1,90 @@
 <?php
-
-
-
 use Phalcon\Mvc\Controller;
 
+class CausaController extends Controller {
+	public function get($id) {
+		$this->response->setContentType("application/json");
+		$causa = Causa::find ( "id=" . $id )->getFirst ();
+		if (! empty ( $causa ))
+			$this->response->setContent ( json_encode ( $causa ) );
+		else
+			$this->response->setStatusCode ( "404" );
+		return $this->response;
+	}
 
-class CausaController extends Controller{
+	public function getAll() {
+		$this->response->setContentType("application/json");
+		$causas = Causa::find ();
+		if (! empty ( $causas->getFirst() )) {
+			$data = array();
+			foreach ( $causas as $causa ) {
+				$data [] = $causa;
+			}
+			$this->response->setContent ( json_encode ( $data ) );
+		} else
+			$this->response->setStatusCode ( "404" );
+		return $this->response;
+	}
 
+	public function post() {
+		$this->response->setContentType("application/json");
+		$causa = new Causa();
+		$json = $this->request->getJsonRawBody ();
+		foreach ( $json as $key => $value ) {
+			$causa->{$key} = $value;
+		}
+		if ($causa->save () == false) {
+			$i = 0;
+			$this->response->setStatusCode ( "409" );
+			foreach ( $causa->getMessages () as $message ) {
+				$data [++$i] = $message->getMessage ();
+			}
+				$this->response->setContent ( json_encode ( $data ) );
+	} else
+			$this->response->setStatusCode ( "204" );
 
-public function getAll(){
-  $causasfound = Causa::find();
-//  $causasfound->AreaAtendimento;
-  foreach ($causasfound as $causa){
-      $causa->AreaAtendimento;
-      //cria array de causas
-      $causas[]=$causa;
-  }
-  echo json_encode($causas);
-}
-
-public function post(){
-  $value=$this->request->getJsonRawBody();
-  $value->hue="worked";
-  echo json_encode($value);
-}
-
-public function getOne($id){
-  echo json_encode("hue".$id);
-}
-
+		return $this->response;
+	}
+	public function put($id){
+		$this->response->setContentType("application/json");
+		$json = $this->request->getJsonRawBody ();
+		$causa = Causa::find ( "id=" . $id )->getFirst ();
+		if (empty ( $causa ))
+			$causa = new Questao ();
+		foreach ( $json as $key => $value ) {
+			$causa->{$key} = $value;
+		}
+		if ($causa->save () == false) {
+			$i = 0;
+			$this->response->setStatusCode ( "409" );
+			foreach ( $causa->getMessages () as $message ) {
+				$data [++$i] = $message->getMessage ();
+			}
+			$this->response->setContent ( json_encode ( $data ) );
+		} else {
+			if (! empty ( $causa ))
+				$this->response->setStatusCode ( "204" );
+			else
+				$this->response->setStatusCode ( "201" );
+		}
+		return $this->response;
+	}
+	public function delete($id){
+		$this->response->setContentType("application/json");
+		$causa = Causa::find ( "id=" . $id )->getFirst ();
+		if (! empty ( $causa )) {
+			try {
+				$causa->delete ();
+				$this->response->setStatusCode ( "204" );
+			} catch ( Exception $e ) {
+				$this->response->setStatusCode ( "409" );
+				foreach ( $causa->getMessages () as $message ) {
+					$data [++$i] = $message->getMessage ();
+				}
+				$this->response->setContent ( json_encode ( $data ) );
+			}
+		} else
+			$this->response->setStatusCode ( "404" );
+		return $this->response;
+	}
 }
